daf3bd012e3a8252a0622e9c3f153c5efce5f043
Gurfh <yoshimitsu@bluewin.ch>
2026-01-21 09:42:30 +0100

    Import TimeoutException in MdRequester.java
    Added import statement for TimeoutException.

--- src/main/java/com/trdp/md/MdRequester.java
+++ src/main/java/com/trdp/md/MdRequester.java
@@ -19,6 +19,7 @@ import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.CountDownLatch; // Import CountDownLatch
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class MdRequester implements AutoCloseable {
